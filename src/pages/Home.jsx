998598327960b7f2918bc8baab81d7621f96ed13
Davi Amaral <40430859+daviamarall@users.noreply.github.com>
2025-06-01 23:17:38 +0000

    react portifolio update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,10 @@ function Home() {
   return (
     <section className="h-screen flex flex-col justify-center items-center">
       <h1 className="text-5xl font-bold mb-4">Prazer, eu sou o Davi! </h1>
-      <p className="text-lg">Desenvolvedor Front-end | React | JavaScript | Tailwind</p>
+      <p className="text-lg">
+  Desenvolvedor Full-Stack | React | JavaScript | Node.js | Express | MongoDB | MySQL | Git | Docker | AWS 
+</p>
+
     </section>
   );
 }
