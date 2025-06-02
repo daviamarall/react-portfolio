b8b515a3a2e4a3d46bf43c6783e01a73d7f4ca0e
Davi Amaral <40430859+daviamarall@users.noreply.github.com>
2025-06-02 00:52:37 +0000

    react portifolio update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ function Home() {
     <section className="py-32 flex flex-col justify-center items-center">
       <div className="flex flex-col justify-center items-center px-4">
         <h1 className="text-5xl font-bold mb-4">Prazer, eu sou o Davi!</h1>
-        <p className="whitespace-nowrap text-center text-sm sm:text-base">
+        <p className="whitespace-nowrap text-center text-sm sm:text-base overflow-x-auto">
           Software Engineer | React | JavaScript | Node.js | Express | MongoDB | SQL | Python | Github | AWS | Docker | Cloud | DevOps Enthusiast
         </p>
       </div>
