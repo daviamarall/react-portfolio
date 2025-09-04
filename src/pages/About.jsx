2225c0608a5bae7885a1e022e7fe446deccb731c
Davi Amaral <40430859+daviamarall@users.noreply.github.com>
2025-09-04 13:21:46 +0000

    update sobre

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,16 @@ function About() {
   return (
     <section className="p-10">
       <h2 className="text-3xl mb-4">Sobre Mim</h2>
-      <p>Desenvolvedor apaixonado por tecnologia, com foco em React, Node.js e tecnologias modernas. Graduado em Análise e desenvolvimento de sistemas, MBA em Gestão de projetos, e Business Intelligence. Atuo na TI desde 2011, passei por diversos segmentos e projetos que me renderam diversas experiências enrriquecedoras. Busco sempre agregar valor ao que faço, aprender novas tecnologias, compartilhar conhecimento e ideias.</p>
+      <p>
+        Apaixonado por tecnologia, sou formado em Análise e Desenvolvimento de
+        Sistemas, com pós-graduação em Analytics e Business Intelligence e MBA
+        em Gestão de Projetos. Desde 2011, atuo em projetos de tecnologia,
+        acumulando experiências em diversos segmentos que enriqueceram minha
+        trajetória profissional. Tenho paixão por aprender novas tecnologias,
+        compartilhar conhecimento e desenvolver soluções que gerem impacto
+        positivo. Meu foco é entregar valor em tudo que faço, liderando projetos
+        com eficiência e promovendo colaboração e inovação em equipe.
+      </p>
     </section>
   );
 }
